2ef937f1eec8e503ff9430c2f6e9be1f20304b98
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-11-22 17:15:16 +0900

    feat(server): change type of Kanji

--- server/src/kanji.rs
+++ server/src/kanji.rs
@@ -1,10 +1,16 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum KanjiDifficulty {
+    Easy,
+    Medium,
+    Hard,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Kanji {
     pub id: u32,
     pub yomi: String,
-    pub hen: String,
-    pub tsukuri: String,
     pub kanji: char,
+    pub difficulty: KanjiDifficulty,
 }
